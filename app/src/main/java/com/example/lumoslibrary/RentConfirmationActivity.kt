80e1caad66c5f4119e8ceb2e0ca92cea7eaf3050
Nathan Serrano <115672797+naleono@users.noreply.github.com>
2025-02-13 15:21:09 -0800

    feat: initial set up of activities/XML pages

---
+++ app/src/main/java/com/example/lumoslibrary/RentConfirmationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lumoslibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RentConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rent_confirmation)
+    }
+}
---
+++ app/src/main/java/com/example/lumoslibrary/TapSwipeInsertPaymentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lumoslibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TapSwipeInsertPaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tap_swipe_insert_payment)
+    }
+}
