0f693bbb616d9aead60feadd04d51bc83cb36b83
Nathan Serrano <115672797+naleono@users.noreply.github.com>
2025-02-13 18:52:31 -0800

    feat: Initial Help Page set up and help button navigation on MainActivity

--- app/src/main/java/com/example/lumoslibrary/MainActivity.kt
+++ app/src/main/java/com/example/lumoslibrary/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.lumoslibrary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Tapping Help Button navigates to Help Page
+        val helpButton = findViewById<ImageView>(R.id.help_button)
+        helpButton.setOnClickListener{
+            val intent = Intent(this@MainActivity, HelpPageActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
