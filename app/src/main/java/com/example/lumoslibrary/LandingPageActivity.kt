0ca6884ecceb85b985eeeb163d3226c6c1044f9f
Ngoc Huynh <ngocvh@uci.edu>
2025-02-21 13:26:05 -0800

    feat: link landing page to login page

--- app/src/main/java/com/example/lumoslibrary/LandingPageActivity.kt
+++ app/src/main/java/com/example/lumoslibrary/LandingPageActivity.kt
@@ -1,20 +1,38 @@
 package com.example.lumoslibrary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.constraintlayout.motion.widget.MotionLayout
 
 class LandingPageActivity : AppCompatActivity() {
 
 //    private lateinit var motionLayout: MotionLayout
 
+    private lateinit var rentButton: Button
+    private lateinit var returnButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
 
+        rentButton = findViewById(R.id.rent_button)
+        returnButton = findViewById(R.id.return_button)
+
+        HelpButton(this)
+
         // Find views
 //        motionLayout = findViewById(R.id.motion_layout_books)
 
-        HelpButton(this)
+        rentButton.setOnClickListener { navigateToLogin(1) }
+        returnButton.setOnClickListener { navigateToLogin(2) }
     }
+
+    private fun navigateToLogin(state: Int) {
+        CurrentSession.state = state
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
 }
